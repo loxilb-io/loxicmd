a1b13578d525c684d7cea4659359bd9b03793d82
inho <inhogog2@netlox.io>
2022-12-07 11:27:50 +0900

    Added FDB Add/Delete command

---
+++ cmd/create/create_fdb.go
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022 NetLOX Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package create
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"loxicmd/pkg/api"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCreateFDBCmd(restOptions *api.RESTOptions) *cobra.Command {
+	var createFDBCmd = &cobra.Command{
+		Use:   "fdb <MacAddress> <DeviceName>",
+		Short: "Create a FDB",
+		Long: `Create a FDB using LoxiLB. It is working as "bridge fdb add <MacAddress> dev <device>"
+		
+`,
+
+		Run: func(cmd *cobra.Command, args []string) {
+			var FDBMod api.FDBMod
+			// Make FDBMod
+			if err := ReadCreateFDBOptions(&FDBMod, args); err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+				return
+			}
+			resp, err := FDBAPICall(restOptions, FDBMod)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				PrintCreateLbResult(resp, *restOptions)
+				return
+			}
+
+		},
+	}
+
+	return createFDBCmd
+}
+
+func ReadCreateFDBOptions(o *api.FDBMod, args []string) error {
+	if len(args) > 2 {
+		return errors.New("create FDB command get so many args")
+	} else if len(args) <= 1 {
+		return errors.New("create FDB need <MacAddress>  args")
+	}
+
+	if _, err := net.ParseMAC(args[0]); err != nil {
+		return fmt.Errorf("MacAddress '%s' is invalid format", args[0])
+	}
+	o.MacAddress = args[0]
+	o.Dev = args[1]
+
+	return nil
+}
+
+func FDBAPICall(restOptions *api.RESTOptions, FDBModel api.FDBMod) (*http.Response, error) {
+	client := api.NewLoxiClient(restOptions)
+	ctx := context.TODO()
+	var cancel context.CancelFunc
+	if restOptions.Timeout > 0 {
+		ctx, cancel = context.WithTimeout(context.TODO(), time.Duration(restOptions.Timeout)*time.Second)
+		defer cancel()
+	}
+
+	return client.FDB().Create(ctx, FDBModel)
+}
---
+++ cmd/delete/delete_fdb.go
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2022 NetLOX Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package delete
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"loxicmd/pkg/api"
+
+	"github.com/spf13/cobra"
+)
+
+func DeleteFDBValidation(args []string) error {
+	if len(args) > 3 {
+		fmt.Println("delete FDB command get so many args")
+	} else if len(args) <= 1 {
+		return errors.New("delete IP Address need <MacAddress> <device> args")
+	}
+	if _, err := net.ParseMAC(args[0]); err != nil {
+		return fmt.Errorf("MacAddress '%s' is invalid format", args[0])
+	}
+
+	return nil
+}
+
+func NewDeleteFDBCmd(restOptions *api.RESTOptions) *cobra.Command {
+
+	var deleteFDBCmd = &cobra.Command{
+		Use:   "fdb <MacAddress> <DeviceName>",
+		Short: "Delete a FDB",
+		Long:  `Delete a FDB using MacAddress  in the LoxiLB.`,
+
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := DeleteFDBValidation(args); err != nil {
+				fmt.Println("not valid <MacAddress>")
+				return
+			}
+			MacAddress := args[0]
+			Device := args[1]
+
+			client := api.NewLoxiClient(restOptions)
+			ctx := context.TODO()
+			var cancel context.CancelFunc
+			if restOptions.Timeout > 0 {
+				ctx, cancel = context.WithTimeout(ctx, time.Duration(restOptions.Timeout)*time.Second)
+				defer cancel()
+			}
+			subResources := []string{
+				MacAddress, "dev", Device,
+			}
+			resp, err := client.FDB().SubResources(subResources).Delete(ctx)
+			if err != nil {
+				fmt.Printf("Error: Failed to delete FDB : %s", MacAddress)
+				return
+			}
+			defer resp.Body.Close()
+			fmt.Printf("Debug: response.StatusCode: %d\n", resp.StatusCode)
+			if resp.StatusCode != http.StatusOK {
+				PrintDeleteLbResult(resp, *restOptions)
+				return
+			}
+
+		},
+	}
+
+	return deleteFDBCmd
+}
